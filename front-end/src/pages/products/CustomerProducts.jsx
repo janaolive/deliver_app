7a1fed13e28e9de926e1a1c24b53990234577de1
José Alexandre Filho <josealexandre301428@gmail.com>
2022-09-27 12:37:49 -0300

    Feat: implementando logica de rota estatica para fotos dos produtos

--- front-end/src/pages/products/CustomerProducts.jsx
+++ front-end/src/pages/products/CustomerProducts.jsx
@@ -1,14 +1,54 @@
-/* import React, { useState, useContext } from 'react';
-import UserContext from '../../context/user/context';
-import api from '../../services/Api'; */
+import React, { useState, useEffect } from 'react';
+import { Card, CardBody, CardTitle } from 'reactstrap';
+
+import api from '../../services/Api';
 import NavBar from './components/NavBar';
 
 export default function CustomProducts() {
+  const [products, setProducts] = useState([]);
+  const [showError, setError] = useState(false);
+
+  function makeProducts(product) {
+    return (
+      <Card
+        key={ product.id }
+        style={ {
+          width: '18rem',
+        } }
+      >
+        <img
+          alt={ product.name }
+          src={ product.url_image }
+        />
+        <CardBody>
+          <CardTitle tag="h5">
+            { product.name }
+          </CardTitle>
+        </CardBody>
+      </Card>
+    );
+  }
+
+  const handleFetch = async () => {
+    try {
+      const product = await api.get('/customer/products');
+      console.log(product);
+      setProducts(product.data);
+    } catch (error) {
+      setError(true);
+    }
+  };
+
+  useEffect(() => {
+    handleFetch();
+  }, []);
   return (
     <main>
       <NavBar />
       <div>
-        produtos aqui
+        { showError
+          ? null
+          : products.map((product) => makeProducts(product)) }
       </div>
     </main>
   );
