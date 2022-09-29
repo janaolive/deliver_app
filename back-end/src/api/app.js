0c474679b3bd323361e61004cc24d0e530244813
Anjel Diniz <anjeldiniz@gmail.com>
2022-09-28 21:48:00 -0300

    Merge branch 'main-group-11' into 'main-group-11-customer'

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const cors = require('cors');
 require('express-async-errors');
+<<<<<<< HEAD
 
 const loginRouter = require('../routers/loginRouter');
 const registerRouter = require('../routers/registerRouter');
 const customerProductsRouter = require('../routers/customerProductsRouter');
+=======
+const cors = require('cors');
+const loginRouter = require('../routers/loginRouter');
+const registerRouter = require('../routers/registerRouter');
+const customerRouter = require('../routers/customerRouter');
+// const { errorsMiddleware } = require('./middleware/errorsMiddleware');
+>>>>>>> 8d6a6e34... test about jwt
 
 const app = express();
 app.use(express.json());
@@ -13,8 +20,12 @@ app.use(cors());
 
 app.use('/', loginRouter);
 app.use('/register', registerRouter);
+<<<<<<< HEAD
 app.use('/customer/products', customerProductsRouter);
 
+=======
+app.use('/customer', customerRouter);
+>>>>>>> 8d6a6e34... test about jwt
 app.get('/coffee', (_req, res) => res.status(418).end());
 
 // app.use(errorsMiddleware);
