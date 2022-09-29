e76b3cba6712eba9a486f0e08e04aa189246d7e1
Janaina P de Oliveira Minutti <janainapp@gmail.com>
2022-09-29 20:41:05 -0300

    feat: tests - register, user, sale e products

---
+++ back-end/src/tests/products.js
@@ -0,0 +1,9 @@
+// const sinon = require('sinon');
+// const chai = require ('chai');
+// const { User } = require ('../database/models'); 
+// const chaiHttp = require('chai-http')
+// const app = require ('../api/app');
+
+// chai.use(chaiHttp);
+
+// const { expect } = chai;
